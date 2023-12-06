8dec5c1e5638499f04129cd4be17cc293c41165f
gitbyways <valievroman.bt@gmail.com>
2023-12-06 10:07:36 +0100

    shape zu player geändert Position von Player gesetzt

--- ST1-Shooter-Royal/main.cpp
+++ ST1-Shooter-Royal/main.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Shooter-Royal");
-    sf::CircleShape shape(25.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::CircleShape player(25.f);
+    player.setPosition(920.f, 500.f);
+    player.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -17,26 +18,26 @@ int main()
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         {
-            shape.move(-1.f, 0.f);
+            player.move(-1.f, 0.f);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            shape.move(1.f, 0.f);
+            player.move(1.f, 0.f);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
         {
-            shape.move(0.f, -1.f);
+            player.move(0.f, -1.f);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         {
-            shape.move(0.f, 1.f);
+            player.move(0.f, 1.f);
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
     
